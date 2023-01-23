a87ed8bb1786e23db08d280c2d3aa029fc9a7d12
sleepyjoyy <sleepyjoyy@gmail.com>
2023-01-24 02:42:28 +0900

    Remove: 소스파일 변경

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import bg from "./bg.jpg";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import TodoTemplate from "./component/TodoTemplate";
+import TodoNav from "./component/TodoNav";
+import TodoHead from "./component/TodoHead";
+import TodoList from "./component/TodoList";
+import TodoCreate from "./component/TodoCreate";
+
+const GlobalStyle = createGlobalStyle`
+body {
+  background-image: url(${bg});
+}
+`;
 
 function App() {
+  let [todos, setToods] = useState([
+    {
+      id: 1,
+      text: "상세페이지 UX개선",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "강아지 산책",
+      checked: false,
+    },
+    {
+      id: 3,
+      text: "줌미팅 10AM",
+      checked: false,
+    },
+  ]);
+
+  let left = todos.filter((todo) => todo.checked === false).length;
+
+  const onCreateTodo = (text) => {
+    if (text === "") {
+      return alert("할 일을 입력해 주세요");
+    } else {
+      const todo = {
+        id: todos.length + 1,
+        text,
+        checked: false,
+      };
+      setToods((todos) => todos.concat(todo));
+    }
+  };
+
+  const onCheck = (id) => {
+    setToods((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <TodoTemplate>
+          <TodoNav />
+          <TodoHead ttlTodo={todos.length} leftTodo={left} />
+          <TodoList todos={todos} onCheck={onCheck} />
+          <TodoCreate onCreateTodo={onCreateTodo} />
+        </TodoTemplate>
+      </div>
+    </>
   );
 }
 
